0e7472d653dcc73e2c802e64bfaa3b9725061dfa
Mykolas <mykolas.kucinskas2@gmail.com>
2022-11-08 09:42:38 +0200

    fixed directories and structure

--- src/pages/EmployeesListPage.tsx
+++ src/pages/EmployeesListPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const EmployeesListPage = () => {
     return (
         <>
-        <p>Here should be a list of employees in here. This functionality is nat added yet.</p>
+            <p>Here should be a list of employees in here. This functionality is nat added yet.</p>
         </>
     );
 }
