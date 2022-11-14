4204343fde70c36f8b5a465f45d51ed9f53220d6
Mykolas <mykolas.kucinskas2@gmail.com>
2022-11-14 21:59:45 +0200

    style: added some styles and added header, footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,32 @@ import EmployeesListPage from './pages/EmployeesListPage';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
 import NewEmployeePage from './pages/NewEmployeePage';
+import './App.css';
 
 const App = () => {
     return(
         <>
-        <nav>
-            <Link to="/">Home Page</Link>
-            <Link to="/list">Employees List</Link>
-            <Link to="/new">New Employee</Link>
+        <header>
+        <h1 className='header-h1'>Employee Management Application</h1>
+        <nav className='header-nav-links'>
+            <button><Link  to="/">Home Page</Link></button>
+            <button><Link  to="/list">Employees List</Link></button>
+            <button><Link  to="/new">New Employee</Link></button>
         </nav>
+
+        </header>
+
 
         <Routes>
             <Route path='/' element={<HomePage/>}></Route>
             <Route path='*' element={<ErrorPage/>}></Route>
             <Route path='/list' element={<EmployeesListPage/>}></Route>
             <Route path='/new' element={<NewEmployeePage/>}></Route>
-        </Routes>     
+        </Routes>
+
+        <footer >
+            Created by Mykoliux. All rights reserved 2022
+        </footer>     
         </>
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,13 +3,19 @@ import React from 'react';
 const HomePage = () => {
     return(
         <>
-        <p> Welcome to the Employee Management Application.</p>
-        <p>This is an application that is intended for:</p>
-        <ul>
-            <li>Reviewing relevant information about the employee,</li>
-            <li>Adding new employee to the employee list,</li>
-            <li>Checking all the employees from the list.</li>
-        </ul>
+        
+
+        <section>
+
+        </section>
+
+        <section>
+
+        </section>
+
+        <section>
+
+        </section>
         </>
     )
 }
